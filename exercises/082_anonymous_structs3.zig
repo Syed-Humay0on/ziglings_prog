dae328c184409f77f05040becf0381158f91a547
Proto <Humayoon04syed@gmail.com>
2026-01-24 16:57:08 +0500

    ✔ 082 [tuples: Anonymous Structs]

--- exercises/082_anonymous_structs3.zig
+++ exercises/082_anonymous_structs3.zig
@@ -57,7 +57,7 @@ pub fn main() void {
 // Let's make our own generic "tuple" printer. This should take a
 // "tuple" and print out each field in the following format:
 //
-//     "name"(type):value
+    // "name"(type):value
 //
 // Example:
 //
@@ -82,14 +82,14 @@ fn printTuple(tuple: anytype) void {
     //         @typeInfo(Circle).@"struct".fields
     //
     // This will be an array of StructFields.
-    const fields = ???;
+    const fields = @typeInfo(@TypeOf(tuple)).@"struct".fields;
 
     // 2. Loop through each field. This must be done at compile
     // time.
     //
     //     Hint: remember 'inline' loops?
     //
-    for (fields) |field| {
+    inline for (fields) |field| {
         // 3. Print the field's name, type, and value.
         //
         //     Each 'field' in this loop is one of these:
@@ -119,9 +119,9 @@ fn printTuple(tuple: anytype) void {
         //
         // The first field should print as: "0"(bool):true
         print("\"{s}\"({any}):{any} ", .{
-            field.???,
-            field.???,
-            ???,
+            field.name,
+            field.type,
+            @field(tuple, field.name),
         });
     }
 }
